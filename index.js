ef93cf89818bcf774d0089b5c81e70f289432403
LeeKangRyong <gaiogo2@naver.com>
2025-07-24 14:39:42 +0900

    0.0.0.0 추가로 모바일에서 접근 허용시키기

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const config = require('./src/config/config');
 
-app.listen(config.port, () => {
+app.listen(config.port, '0.0.0.0', () => {
     console.log(`http://localhost:${config.port}`);
 });
